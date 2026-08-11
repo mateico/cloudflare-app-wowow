4438284b601c84e5dd94e586cdfca4554c007175
Mateo Rial <mateo.rial@gmail.com>
2026-08-11 19:09:33 -0300

    new test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">Deploy to Cloudflar.</li>
           <li className="tracking-[-.01em]">Preview Deployment .</li>
-          <li className="tracking-[-.01em]">This is a new feature4.</li>
+          <li className="tracking-[-.01em]">This is a new feature5.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
